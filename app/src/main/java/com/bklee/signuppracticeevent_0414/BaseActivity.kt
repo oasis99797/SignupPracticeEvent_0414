fabbc2ff1d34355a7931828118bb193637474142
oasis99797 <oasis99797@gmail.com>
2020-04-14 20:41:58 +0900

    Initial commit

---
+++ app/src/main/java/com/bklee/signuppracticeevent_0414/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.bklee.signuppracticeevent_0414
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+
+
+
+}
