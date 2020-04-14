8afa9c92e7da3e15b0bfd1d5c8b206d907fa79b5
oasis99797 <oasis99797@gmail.com>
2020-04-14 21:35:06 +0900

    리니어 레이아웃의 클릭 이벤트

--- app/src/main/java/com/bklee/signuppracticeevent_0414/MainActivity.kt
+++ app/src/main/java/com/bklee/signuppracticeevent_0414/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,6 +17,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        리니어 레이아웃의 클릭 이벤트
+
+        signUpBtn.setOnClickListener {
+
+            Toast.makeText(mContext, "회원가입 시도합니다.", Toast.LENGTH_SHORT).show()
+
+        }
 
 //        전체동의 체크박스. : 체크 되면 나머지를 전부 체크, 해제되면 모두 해제
 
