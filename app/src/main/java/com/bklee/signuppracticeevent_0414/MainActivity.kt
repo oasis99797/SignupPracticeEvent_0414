3baf272f785eb015d7030a128d51ed5f4ebc218f
oasis99797 <oasis99797@gmail.com>
2020-04-14 21:04:38 +0900

    EditText 내용 변경 감지 이벤트 처리

--- app/src/main/java/com/bklee/signuppracticeevent_0414/MainActivity.kt
+++ app/src/main/java/com/bklee/signuppracticeevent_0414/MainActivity.kt
@@ -2,11 +2,51 @@ package com.bklee.signuppracticeevent_0414
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+        idEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal = s.toString()
+                if (inputVal.length == 0) {
+                    idStatusTxt.text = "최소 여섯글자 이상으로 해주세요."
+                }
+                else if (inputVal.length < 6) {
+                    idStatusTxt.text = "길이가 너무 짧습니다."
+                }
+                else {
+                    idStatusTxt.text = "사용해도 좋은 아이디입니다."
+                }
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
 }
